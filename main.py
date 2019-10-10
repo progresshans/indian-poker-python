c0da0127fdf29f4cd5ae587ad937b6fe67eba9f2
progresshans <progresshans@gmail.com>
2019-10-10 20:07:55 +0900

    PyQt5 사용을 위한 기타 셋팅들

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import sys
 import importlib
 from PyQt5.QtWidgets import *
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 def load_module(module_name):
     mod = importlib.import_module(module_name)
@@ -17,13 +18,38 @@ class GameStart:
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        wg = MainPageWidget()
-        self.setCentralWidget(wg)
+        pswg = PreviousSettingPage()
+        self.setCentralWidget(pswg)
+
         self.statusBar().showMessage('원본 코드 :  https://github.com/progresshans/indian-poker-python')
         self.setGeometry(200,200,1000,700)
         self.show()
+    
+    def change_page(self):
+        if value == 0:
+            wg = PreviousSettingPage()
+            setCentralWidget(wg)
+        elif value == 1:
+            wg = MainPage()
+            self.setCentralWidget(self.wg)
 
 class PreviousSettingPage(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Indian Poker Setting')
+        self.resize(1000, 700)
+        self.initUI()
+        self.show()
+
+    def initUI(self):
+        btn = QPushButton('확인', self)
+        btn.move(10, 10)
+        btn.resize(btn.sizeHint())
+        btn.clicked.connect(self.enter_clicked)
+
+    def enter_clicked(self):
+        MainWindow.change_page(1)
+
 
 class MainPage(QWidget):
     def __init__(self):
@@ -31,6 +57,7 @@ class MainPage(QWidget):
         self.setWindowTitle('Indian Poker')
         self.resize(1000, 700)
         self.initUI()
+        self.show()
 
     def initUI(self):
         btn = QPushButton('Quit', self)
@@ -38,7 +65,6 @@ class MainPage(QWidget):
         btn.resize(btn.sizeHint())
         btn.clicked.connect(QCoreApplication.instance().quit)
 
-        self.show()
 
 
 if __name__ == '__main__':
